55e71226f668dd00de724c65a01fc2d3bca284ae
Matt <86723332+mattmc86@users.noreply.github.com>
2022-01-20 15:31:38 +0000

    Working on viewing and adding functions

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,19 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const inquirer = require('inquirer');
 
 
 //prompts
-
 const promptUser = () => {
     inquirer.prompt([
         {
@@ -47,14 +46,135 @@ const promptUser = () => {
         if (choices === 'Update an Employee'){
             updateEmployee();
         }
-        if (choices === 'Exit'){
-            //close app TODO
-        }
-
+        // if (choices === 'Exit'){
+        //     //close app TODO
+        // }
 
     })
 }
 
 //functions for each choice
 
+const viewAllDepartments = () =>{
+    let sqlQuery = `SELECT department.id, department.department_name
+                 FROM department;`
+    connection.promise().query(sqlQuery,(error,response)=>{
+        if (error) throw error;
+        console.table(response);
+        promptUser();
+    })
+    
+}
 
+const viewAllRoles= () =>{
+    let sqlQuery = `SELECT role.title, department.department_name, role.id, role.salary
+                 FROM role
+                 INNER JOIN department ON role.department_id=department.id;`
+    connection.promise().query(sqlQuery,(error,response)=>{
+        if (error) throw error;
+        console.table(response);
+        promptUser();
+    })
+    
+}
+
+const viewAllEmployees= () =>{
+    //below query needs to be changed, returns too many results
+    let sqlQuery = `SELECT employee.id, employee.first_name, employee.last_name, employee.role_id ,employee.manager_id, role.salary, department.id
+                 FROM employee, department, role
+                 WHERE department.id = role.department_id
+                 AND role_id = employee.role_id;`
+
+                
+    connection.promise().query(sqlQuery,(error,response)=>{
+        if (error) throw error;
+        console.table(response);
+        promptUser();
+    });
+    
+};
+
+//Add functions
+
+const addDepartment = () =>{
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "newDepartment",
+            message: "What is the name of the new department?"
+            // validation if left blank
+        }
+    ])
+    .then((answer)=>{
+        let sqlQuery = `INSERT INTO department (department_name) VALUE (newDepartment);`;
+        connection.query(sqlQuery, answer.newDepartment,(error,response) =>{
+            if (error) throw error;
+            viewAllDepartments();
+        })
+    })
+}
+
+const addRole = () =>{
+    // variable for all departments
+    inquirer.prompt([
+        {
+            type:"list",
+            name: "currentDepartments",
+            message: "Which department will the role be part of?",
+            choices: 'allDepartsVariable'
+        },
+        {
+            type: "input",
+            name: "newRole",
+            message: "What is the name of the new role?"
+            //validate if left blank
+        },
+        {
+            type: "input",
+            name: "newSalary",
+            message: "What is the salary for the new role?"
+            //validate if left blank
+        }
+    ])
+    .then((answers) =>{
+        let sqlQuery = `INSERT INTO role VALUES (newRole, newSalary, currentDepartments);`;
+        connection.query(sqlQuery, answer.newDepartment,(error,response) =>{
+            if (error) throw error;
+            viewAllDepartments();
+        })
+    })
+}
+
+const addEmployee = () =>{
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Please enter employees first name'
+            // look at validation if left blank
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Please enter employees last name'
+            //look at validation if left blank
+        }
+
+        //need array of roles and save in const 
+        //SELECT role.title FROM role;
+        //{
+        //type:"list",
+        //name: "role",
+        //message: "What is the employees role?",
+        //choices: roles //variable created from roles array
+
+         //need array of managers and save in const 
+        //SELECT employee.first_name, employee.last_name FROM employees;
+        //{
+        //type:"list",
+        //name: "manager",
+        //message: "What is the employees manager?",
+        //choices: managers //variable created from managers array
+
+    ])
+}
